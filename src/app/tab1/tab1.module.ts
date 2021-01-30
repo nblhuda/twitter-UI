ce9d627d22bcf8109e58e71b44c5920c052c118b
User <user@Users-MacBook-Pro.local>
2021-01-30 09:23:32 +0800

    first commit

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+ 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
-
+import { SharedComponentsModule } from '../components/shared-components.module';
+import { SharedDirectivesModule } from '../directives/shared-directives.module';
+ 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    SharedComponentsModule,
+    SharedDirectivesModule
   ],
   declarations: [Tab1Page]
 })
